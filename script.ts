b15e29e506f7b3249394f933a9eff135d863e249
D. Kushnir <student95tnu@gmail.com>
2023-09-23 15:28:53 +0300

    Code style fix

--- script.ts
+++ script.ts
@@ -1,35 +1,42 @@
 type ArticleData = {
-	content: string;
-	title: string;
+  content: string;
+  title: string;
 }
 
 const downloadElement = document.getElementById('download');
 
 downloadElement?.addEventListener('click', async () => {
-	const selectors: HTMLInputElement | null = document.querySelector('#selectors');
-	chrome.storage.sync.set({ selectors: selectors?.value });
-	const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-	const tabId = tab?.id;
+  const selectors = document.querySelector<HTMLInputElement>('#selectors');
+  chrome.storage.sync.set({ selectors: selectors?.value || '' });
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+  const tabId = tab?.id;
 
-	if (tabId) {
-		chrome.scripting.executeScript({
-			target: { tabId },
-			func: downloadArticle,
-		});
-	}
+  if (tabId) {
+    chrome.scripting.executeScript({
+      target: { tabId },
+      func: downloadArticle,
+    });
+  }
 });
 
-const downloadArticle = () => chrome.storage.sync.get('selectors').then(({ selectors: selectors }) => {
-	(selectors as string).split(' ').forEach(selector =>
-		document.querySelectorAll(selector).forEach(element => element.remove()));
+const downloadArticle = () =>
+  chrome.storage.sync.get('selectors').then(({ selectors }) => {
+    const selectorsList = (selectors as string).split(' ');
 
-	chrome.runtime.sendMessage<ArticleData>({ content: document.documentElement.outerHTML, title: document.title });
-});
+    selectorsList.forEach(selector =>
+      document.querySelectorAll(selector).forEach(element => element.remove()));
+
+    chrome.runtime.sendMessage<ArticleData>(
+      { content: document.documentElement.outerHTML, title: document.title }
+    );
+  });
 
 chrome.runtime.onMessage.addListener((articleData: ArticleData) =>
-	chrome.downloads.download({
-		url: URL.createObjectURL(new Blob([articleData.content], { type: 'text/html' })),
-		filename: articleData.title + '.html',
-		saveAs: true
-	})
+  chrome.downloads.download({
+    url: URL.createObjectURL(
+      new Blob([articleData.content], { type: 'text/html' })
+    ),
+    filename: articleData.title + '.html',
+    saveAs: true
+  })
 );
